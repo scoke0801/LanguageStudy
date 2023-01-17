95a04f5ac311ce408e20d1d05058aa6da7ede7b9
scoke0801 <scoke0801@gmail.com>
2023-01-17 23:26:40 +0900

    15596

---
+++ Algorithm/CodingTest/15996.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long long sum(std::vector<int>& a)
+{
+	long long ans = 0;
+	for (int n : a) {
+		ans += n;
+	}
+	return ans;
+}
+int main()
+{
+	
+
+}
