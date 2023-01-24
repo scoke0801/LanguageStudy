599cb727e579abe58b582ec184d29fbc5ddb5b55
scoke0801 <scoke0801@gmail.com>
2023-01-24 20:25:51 +0900

    1152

--- Algorithm/CodingTest/1018.cpp
+++ Algorithm/CodingTest/1018.cpp
@@ -1,53 +1,53 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-char board[50][50];
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	int M, N;
-	cin >> N >> M;
-
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < M; ++j) {
-			cin >> board[i][j];
-		}
-	}
-
-	// 모두 바꾸는 경우.
-	int best = 64;
-	for (int i = 0; i < N - 7; ++i) {
-		for (int j = 0; j < M - 7; ++j) {
-			int toWhite = 0;
-			int toBlack = 0;
-			for (int a = i; a < i + 8; ++a) {
-				for (int b = j; b < j + 8; ++b) {
-					if ((a + b) % 2 == 0) {
-						if (board[a][b] == 'B') {
-							++toWhite;
-						}
-						else {
-							++toBlack;
-						}
-					}
-					else {
-						if (board[a][b] == 'B') {
-							++toBlack;
-						}
-						else {
-							++toWhite;
-						}
-					}
-				}
-			}
-
-			best = std::min(best, toWhite);
-			best = std::min(best, toBlack);
-		}
-	}
-
-	cout << best;
-}
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//
+//char board[50][50];
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//
+//	int M, N;
+//	cin >> N >> M;
+//
+//	for (int i = 0; i < N; ++i) {
+//		for (int j = 0; j < M; ++j) {
+//			cin >> board[i][j];
+//		}
+//	}
+//
+//	// 모두 바꾸는 경우.
+//	int best = 64;
+//	for (int i = 0; i < N - 7; ++i) {
+//		for (int j = 0; j < M - 7; ++j) {
+//			int toWhite = 0;
+//			int toBlack = 0;
+//			for (int a = i; a < i + 8; ++a) {
+//				for (int b = j; b < j + 8; ++b) {
+//					if ((a + b) % 2 == 0) {
+//						if (board[a][b] == 'B') {
+//							++toWhite;
+//						}
+//						else {
+//							++toBlack;
+//						}
+//					}
+//					else {
+//						if (board[a][b] == 'B') {
+//							++toBlack;
+//						}
+//						else {
+//							++toWhite;
+//						}
+//					}
+//				}
+//			}
+//
+//			best = std::min(best, toWhite);
+//			best = std::min(best, toBlack);
+//		}
+//	}
+//
+//	cout << best;
+//}
---
+++ Algorithm/CodingTest/1152.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	std::string str;
+//	getline(cin, str);
+//	
+//	int count = 1;
+//
+//	if (str[0] == ' ' && str.length() == 1)
+//	{
+//		count = 0;
+//	}
+//
+//	for (size_t i = 0; i < str.length(); ++i) {
+//		if (str[i] == ' ') {
+//			if (i == 0 || i == str.length() - 1) { continue; }
+//			++count;
+//		}
+//	}
+//	
+//
+//	cout << count;
+//}
