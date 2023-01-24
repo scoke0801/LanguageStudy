ac33e7ccab4d39e15ebb1e2d6377c3704e00a154
scoke0801 <scoke0801@gmail.com>
2023-01-24 20:31:01 +0900

    11720

---
+++ Algorithm/CodingTest/11720.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//
+//char arr[100];
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//
+//	int N;
+//	cin >> N;
+//	
+//	for (int i = 0; i < N; ++i) {
+//		cin >> arr[i];
+//	}
+//
+//	int sum = 0;
+//	for (int i = 0; i < N; ++i) {
+//		sum += arr[i] - '0';
+//	}
+//
+//	cout << sum;
+//}
