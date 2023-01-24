d8ea876126dc57e811e58985504c27455ade56ad
scoke0801 <scoke0801@gmail.com>
2023-01-24 22:10:21 +0900

    10818

---
+++ Algorithm/CodingTest/10818.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	std::vector<int> arr(N);
+//
+//	for (int i = 0; i < N; ++i) {
+//		cin >> arr[i];
+//	}
+//
+//	cout << *(std::min_element(arr.begin(), arr.end()));
+//	cout << " ";
+//	cout << *(std::max_element(arr.begin(), arr.end()));
+//}
--- Algorithm/CodingTest/5556.cpp
+++ Algorithm/CodingTest/5556.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-using namespace std;
-
-int color[3] = { 1,2,3 };
-int main__()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	
-	int N, K;
-
-	cin >> N >> K;
-
-	int half = N / 2;
-
-	for (int i = 0; i < K; ++i) {	
-		int x, y;
-		cin >> x >> y;
-	
-		if (x > half) {
-			x = N - x + 1;
-		}
-		if (y > half) {
-			y = N - y + 1;
-		}
-		int dist = min(x, y);
-		cout << color[(dist - 1) % 3] << "\n";
-	}
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+//int color[3] = { 1,2,3 };
+//int main__()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	
+//	int N, K;
+//
+//	cin >> N >> K;
+//
+//	int half = N / 2;
+//
+//	for (int i = 0; i < K; ++i) {	
+//		int x, y;
+//		cin >> x >> y;
+//	
+//		if (x > half) {
+//			x = N - x + 1;
+//		}
+//		if (y > half) {
+//			y = N - y + 1;
+//		}
+//		int dist = min(x, y);
+//		cout << color[(dist - 1) % 3] << "\n";
+//	}
+//	return 0;
+//}
