f96188ee4549f0f5f4e4154d4af95363b520721b
scoke0801 <scoke0801@gmail.com>
2023-01-25 20:53:36 +0900

    1085

---
+++ Algorithm/CodingTest/1085.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//
+//int main()
+//{
+//	int x, y, w, h;
+//	cin >> x >> y >> w >> h;
+//
+//	int toEndX = w - x;
+//	int toEndY = h - y;
+//
+//	int best = std::min(toEndX, toEndY);
+//	best = std::min(best, x);
+//	best = std::min(best, y);
+//
+//	cout << best;
+//}
