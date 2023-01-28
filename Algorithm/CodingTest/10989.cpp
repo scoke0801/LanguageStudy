ea99abefc8cd7e7d96f5738ee0cc190c8e5732ed
scoke0801 <scoke0801@gmail.com>
2023-01-28 18:15:58 +0900

    2689 / 10989

---
+++ Algorithm/CodingTest/10989.cpp
@@ -0,0 +1,26 @@
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//
+//	int N;
+//	cin >> N;
+//
+//	vector<int> vec(10001);
+//	for (int i = 0; i < N; ++i) {
+//		int num;
+//		cin >> num;
+//		++vec[num];
+//	}
+//
+//	for (int i = 1; i <= 10000; ++i) {
+//		for (int j = 0; j < vec[i]; ++j) {
+//			cout << i << "\n";
+//		}
+//	}
+//}
---
+++ Algorithm/CodingTest/2869.cpp
@@ -0,0 +1,22 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//
+//	int A, B, V;
+//
+//	cin >> A >> B >> V;
+//
+//	A -= B;
+//	V -= B;
+//
+//	int day = V / A;
+//	if (V % A > 0) {
+//		++day;
+//	}
+//
+//	cout << day << "\n";
+//}
