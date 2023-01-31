0bcdd02390d6e9976244ed60d9a9337dcc7efd3c
scoke0801 <scoke0801@gmail.com>
2023-01-31 22:42:15 +0900

    sort2

--- Algorithm/Algorithm/Sort.cpp
+++ Algorithm/Algorithm/Sort.cpp
@@ -48,13 +48,88 @@ void InsertionSort(vector<int>& v)
 	}
 }
 
+void HeapSort(vector<int>& v)
+{
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	for (int num : v) {
+		pq.push(num);
+	}
+
+	v.clear();
+
+	while (pq.empty() == false) {
+		v.push_back(pq.top());
+		pq.pop();
+	}
+}
+
+// 병합 정렬
+// 분할 정복( Divide And Conquer )
+// - 분할(Divide)	문제를 더 단순하게 분할한다.
+// - 정복(Conquer)	분할된 문제를 해결
+// - 결합(Combine)	결과를 취합하여 마무리
+
+void MergeResult(vector<int>& v, int left, int mid, int right)
+{
+	vector<int> temp;
+
+	int leftIdx = left;
+	int rightIdx = mid + 1;
+
+	while (leftIdx <= mid && rightIdx <= right) {
+		if (v[leftIdx] <= v[rightIdx]) {
+			temp.push_back(v[leftIdx]);
+			++leftIdx;
+		}
+		else {
+			temp.push_back(v[rightIdx]);
+			++rightIdx;
+		}
+	}
+
+	if (leftIdx > mid) {
+		while (rightIdx <= right) {
+			temp.push_back(v[rightIdx++]);
+		}
+	}
+	else {
+		while (leftIdx <= mid) {
+			temp.push_back(v[leftIdx++]);
+		}
+	}
+
+	for (int i = 0; i < temp.size(); ++i) {
+		v[left + i] = temp[i];
+	}
+}
+
+void MergeSort(vector<int>& v, int left, int right)
+{
+	if (left >= right) {
+		return;
+	}
+
+	int mid = (left + right) / 2;
+	MergeSort(v, left, mid);
+	MergeSort(v, mid + 1, right);
+	
+	MergeResult(v, left, mid, right);
+}
+
 int main()
 {
-	vector<int> v{ 5, 1,3,4,2 };
+	srand(time(nullptr));
 	
+	vector<int> v(50);
+	for (int i = 0; i < 50; ++i) {
+		v[i] = (rand() % 100);
+	}
+
 	// BubbleSort(v);
 	// SelectionSort(v);
-	InsertionSort(v);
+//	InsertionSort(v);
+	MergeSort(v, 0, v.size() - 1);
 
 	for (int n : v) {
 		cout << n << "\n";
