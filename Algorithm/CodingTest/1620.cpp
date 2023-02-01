007204a27dc75a0c7c111c1b564f9c33f1b9161a
scoke0801 <scoke0801@gmail.com>
2023-02-01 21:41:31 +0900

    1620

---
+++ Algorithm/CodingTest/1620.cpp
@@ -0,0 +1,36 @@
+//#include <iostream>
+//#include <unordered_map>
+//#include <cctype>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//
+//	int N, M;
+//	cin >> N >> M;
+//
+//	unordered_map<string, int> toNum;
+//	unordered_map<int, string> toName;
+//
+//	for (int i = 1; i <= N; ++i) {
+//		string name;
+//		cin >> name;
+//		toNum[name] = i;
+//		toName[i] = name;
+//	}
+//
+//	for (int i = 0; i < M; ++i) {
+//		string question;
+//		cin >> question;
+//		if (isdigit(question[0])) {
+//			int num = std::stoi(question);
+//			cout << toName[num] << "\n";
+//		}
+//		else {
+//			cout << toNum[question] << "\n";
+//		}
+//	}
+//}
