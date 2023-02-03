73d15d7522a587431a0f6c859f60f0cfaf1522e6
scoke0801 <scoke0801@gmail.com>
2023-02-03 22:34:37 +0900

    1676

---
+++ Algorithm/CodingTest/1676.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//
+//	int N;
+//	cin >> N;
+//
+//	int ans = 0;
+//	for (int i = 5; i <= N; i *= 5) {
+//		ans += N / i;
+//	}
+//
+//	cout << ans << "\n";
+//}
--- Algorithm/CodingTest/17219.cpp
+++ Algorithm/CodingTest/17219.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <unordered_map>
-using namespace std;
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	unordered_map<string, string> data;
-
-	int N, M;
-	cin >> N >> M;
-	for (int i = 0; i < N; ++i) {
-		string addr, pw;
-		cin >> addr >> pw;
-
-		data[addr] = pw;
-	}
-
-	for (int i = 0; i < M; ++i) {
-		string addr;
-		cin >> addr;
-
-		cout << data[addr] << "\n";
-	}
-}
+//#include <iostream>
+//#include <unordered_map>
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//
+//	unordered_map<string, string> data;
+//
+//	int N, M;
+//	cin >> N >> M;
+//	for (int i = 0; i < N; ++i) {
+//		string addr, pw;
+//		cin >> addr >> pw;
+//
+//		data[addr] = pw;
+//	}
+//
+//	for (int i = 0; i < M; ++i) {
+//		string addr;
+//		cin >> addr;
+//
+//		cout << data[addr] << "\n";
+//	}
+//}
