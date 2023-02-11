f239704b1252bfa32b02272b64544c376e8a542b
scoke0801 <scoke0801@gmail.com>
2023-02-11 16:49:53 +0900

    18870 수정

--- Algorithm/CodingTest/18870.cpp
+++ Algorithm/CodingTest/18870.cpp
@@ -1,9 +1,8 @@
 //#include <iostream>
 //#include <vector>
 //#include <algorithm>
-//#include <map>
-//#include <set>
 //using namespace std;
+//
 //int main()
 //{
 //	ios_base::sync_with_stdio(false);
@@ -12,20 +11,17 @@
 //	cin >> N;
 //
 //	vector<int> coords( N );
-//	map<int,int> map;
-//	set<int> set;
 //	for (int i = 0; i < N; ++i) {
 //		cin >> coords[i];
-//		set.insert(coords[i]);
 //	}
 //
-//	int index = 0;
-//	for (int num : set) {
-//		map[num] = index;
-//		++index;
-//	}
-//	
+//	vector<int> copy(coords);
+//
+//	sort(copy.begin(), copy.end());
+//	copy.erase(unique(copy.begin(), copy.end()), copy.end());
+//
 //	for (int coord : coords) {
-//		cout << map[coord] << " ";
+//		auto iter = lower_bound(copy.begin(), copy.end(), coord);
+//		cout << iter - copy.begin() << " ";
 //	}
 //}
