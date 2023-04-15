714f27b921697fe59e573f7f8e189664c3a21aff
scoke0801 <scoke0801@gmail.com>
2023-04-15 14:17:33 +0900

    46

--- Algorithm/CodingTest_Inflearn/45.cpp
+++ Algorithm/CodingTest_Inflearn/45.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <queue>
-using namespace std;
-
-int main()
-{
-	queue<int> princes;
-	int N, K;
-	
-	cin >> N >> K;
-
-	for (int i = 1; i <= N; ++i) {
-		princes.push(i);
-	}
-
-	int count = 0;
-	while ( princes.size() > 1 )
-	{
-		int cur = princes.front();
-		princes.pop();
-
-		++count;
-		if (count != K) {
-			princes.push(cur);
-			continue;
-		}
-
-		count = 0;
-	}
-
-	cout << princes.front();
-}
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//int main()
+//{
+//	queue<int> princes;
+//	int N, K;
+//	
+//	cin >> N >> K;
+//
+//	for (int i = 1; i <= N; ++i) {
+//		princes.push(i);
+//	}
+//
+//	int count = 0;
+//	while ( princes.size() > 1 )
+//	{
+//		int cur = princes.front();
+//		princes.pop();
+//
+//		++count;
+//		if (count != K) {
+//			princes.push(cur);
+//			continue;
+//		}
+//
+//		count = 0;
+//	}
+//
+//	cout << princes.front();
+//}
