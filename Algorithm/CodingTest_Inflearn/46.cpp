22b17f2d363a4704ffcdf93cf13b16077b8d04e8
scoke0801 <scoke0801@gmail.com>
2023-04-16 14:11:07 +0900

    47

--- Algorithm/CodingTest_Inflearn/46.cpp
+++ Algorithm/CodingTest_Inflearn/46.cpp
@@ -1,51 +1,51 @@
-#include <iostream>
-#include <queue>
-
-using namespace std;
-
-struct Work
-{
-	int idx = 0;
-	int time = 0;
-};
-int main()
-{
-	int N;
-	cin >> N;
-
-	int total = 0;
-	queue<Work> queue;
-	for (int i = 1; i <= N; ++i) {
-		int num;
-		cin >> num;
-		queue.push(Work{ i, num });
-	}
-
-	int K;
-	cin >> K;
-
-	while ( !queue.empty() )
-	{
-		Work work = queue.front();
-		queue.pop();
-
-		if (work.time > 1) {
-			work.time--;
-			queue.push(work);
-		}
-
-		++total;
-		if (total == K) {
-			break;
-		}
-	}
-
-	int ans = -1;
-	if (!queue.empty()) {
-		ans = queue.front().idx;
-	}
-
-	cout << ans << "\n";
-
-	
-}
+//#include <iostream>
+//#include <queue>
+//
+//using namespace std;
+//
+//struct Work
+//{
+//	int idx = 0;
+//	int time = 0;
+//};
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	int total = 0;
+//	queue<Work> queue;
+//	for (int i = 1; i <= N; ++i) {
+//		int num;
+//		cin >> num;
+//		queue.push(Work{ i, num });
+//	}
+//
+//	int K;
+//	cin >> K;
+//
+//	while ( !queue.empty() )
+//	{
+//		Work work = queue.front();
+//		queue.pop();
+//
+//		if (work.time > 1) {
+//			work.time--;
+//			queue.push(work);
+//		}
+//
+//		++total;
+//		if (total == K) {
+//			break;
+//		}
+//	}
+//
+//	int ans = -1;
+//	if (!queue.empty()) {
+//		ans = queue.front().idx;
+//	}
+//
+//	cout << ans << "\n";
+//
+//	
+//}
