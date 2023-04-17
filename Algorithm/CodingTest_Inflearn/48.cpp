908e2d8b45f3c5bd038d87fb9d42b81c73e825fe
scoke0801 <scoke0801@gmail.com>
2023-04-17 21:48:41 +0900

    49

--- Algorithm/CodingTest_Inflearn/48.cpp
+++ Algorithm/CodingTest_Inflearn/48.cpp
@@ -1,33 +1,33 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main()
-{
-	int N = 9;
-
-	for (int i = 0; i < N; ++i) {
-		vector<int> vec(N);
-		int total = 0;
-		for (int j = 0; j < N; ++j) {
-			cin >> vec[j];
-			total += vec[j];
-		}
-		
-		int aver = ( static_cast<float>( total ) / N ) + 0.5f;
-		int closer = std::abs( aver - vec[0] );
-		int ans = vec[0];
-		for (int j = 1; j < N; ++j) {
-			int gap = std::abs(aver - vec[j]);
-			if (closer > gap)
-			{
-				closer = gap;
-				ans = vec[j];
-			}
-			else if (closer == gap) {
-				ans = std::max(ans, vec[j]);
-			}
-		}
-		cout << aver << " " << ans << "\n";
-	}
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int N = 9;
+//
+//	for (int i = 0; i < N; ++i) {
+//		vector<int> vec(N);
+//		int total = 0;
+//		for (int j = 0; j < N; ++j) {
+//			cin >> vec[j];
+//			total += vec[j];
+//		}
+//		
+//		int aver = ( static_cast<float>( total ) / N ) + 0.5f;
+//		int closer = std::abs( aver - vec[0] );
+//		int ans = vec[0];
+//		for (int j = 1; j < N; ++j) {
+//			int gap = std::abs(aver - vec[j]);
+//			if (closer > gap)
+//			{
+//				closer = gap;
+//				ans = vec[j];
+//			}
+//			else if (closer == gap) {
+//				ans = std::max(ans, vec[j]);
+//			}
+//		}
+//		cout << aver << " " << ans << "\n";
+//	}
+//}
---
+++ Algorithm/CodingTest_Inflearn/49.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector< vector<int> > vec(N, vector<int>(N));
+
+	for (int i = 0; i < N; ++i) {
+		int num;
+		cin >> num;
+
+		for (int j = 0; j < N; ++j) {
+			vec[j][i] = num;
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i < N; ++i) {
+		int num;
+		cin >> num;
+
+		for (int j = 0; j < N; ++j) {
+			vec[i][j] = std::min(vec[i][j], num);
+			ans += vec[i][j];
+		}
+	}
+
+	cout << ans << "\n";
+}
