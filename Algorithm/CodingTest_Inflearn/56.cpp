801b83b2aa233378ee63ba90f091265883a23319
scoke0801 <scoke0801@gmail.com>
2023-04-26 20:39:59 +0900

    57

--- Algorithm/CodingTest_Inflearn/56.cpp
+++ Algorithm/CodingTest_Inflearn/56.cpp
@@ -1,21 +1,21 @@
-#include <iostream>
-#include <stack>
-
-using namespace std;
-
-int N;
-void print(int num)
-{
-	if (num > N) {
-		return;
-	}
-
-	cout << num << " ";
-	print(num + 1);
-}
-
-int main()
-{
-	cin >> N;
-	print(1);
-}
+//#include <iostream>
+//#include <stack>
+//
+//using namespace std;
+//
+//int N;
+//void print(int num)
+//{
+//	if (num > N) {
+//		return;
+//	}
+//
+//	cout << num << " ";
+//	print(num + 1);
+//}
+//
+//int main()
+//{
+//	cin >> N;
+//	print(1);
+//}
