04826f11934f7c13f32aeb02b2aacd772a1dbef2
scoke0801 <scoke0801@gmail.com>
2023-04-26 20:04:54 +0900

    56

--- Algorithm/CodingTest_Inflearn/55.cpp
+++ Algorithm/CodingTest_Inflearn/55.cpp
@@ -1,59 +1,59 @@
-#include <iostream>
-#include <stack>
-#include <queue>
-using namespace std;
-
-int main()
-{
-	int N;
-	cin >> N;
-
-	queue<int> queueBuf;
-	for (int i = 0; i < N; ++i) {
-		int num;
-		cin >> num;
-		queueBuf.push(num);
-	}
-
-	std::string answer;
-	int cur = 1;
-	stack<int> stackBuf;
-	while  (!queueBuf.empty() )
-	{
-		if (!stackBuf.empty())
-		{
-			int top = stackBuf.top();
-			if (top == cur)
-			{
-				++cur;
-				stackBuf.pop();
-				answer += "O";
-
-				continue;
-			}
-		}
-
-		int front = queueBuf.front();
-		queueBuf.pop();
-		stackBuf.push(front);
-		answer += "P";
-	}
-
-	while (!stackBuf.empty())
-	{
-		int top = stackBuf.top();
-		stackBuf.pop();
-		if (top == cur)
-		{
-			++cur;
-			answer += "O";
-		}
-	}
-
-	if (N == cur - 1) {
-		cout << answer;
-	}
-	else {
-		cout << "impossible";
-	}
-}
+//#include <iostream>
+//#include <stack>
+//#include <queue>
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//
+//	queue<int> queueBuf;
+//	for (int i = 0; i < N; ++i) {
+//		int num;
+//		cin >> num;
+//		queueBuf.push(num);
+//	}
+//
+//	std::string answer;
+//	int cur = 1;
+//	stack<int> stackBuf;
+//	while  (!queueBuf.empty() )
+//	{
+//		if (!stackBuf.empty())
+//		{
+//			int top = stackBuf.top();
+//			if (top == cur)
+//			{
+//				++cur;
+//				stackBuf.pop();
+//				answer += "O";
+//
+//				continue;
+//			}
+//		}
+//
+//		int front = queueBuf.front();
+//		queueBuf.pop();
+//		stackBuf.push(front);
+//		answer += "P";
+//	}
+//
+//	while (!stackBuf.empty())
+//	{
+//		int top = stackBuf.top();
+//		stackBuf.pop();
+//		if (top == cur)
+//		{
+//			++cur;
+//			answer += "O";
+//		}
+//	}
+//
+//	if (N == cur - 1) {
+//		cout << answer;
+//	}
+//	else {
+//		cout << "impossible";
+//	}
+//}
