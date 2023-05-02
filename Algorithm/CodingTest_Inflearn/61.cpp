5233b55994dc9be18736db785b4baabb13ed45c6
scoke0801 <scoke0801@gmail.com>
2023-05-02 22:39:45 +0900

    62

--- Algorithm/CodingTest_Inflearn/61.cpp
+++ Algorithm/CodingTest_Inflearn/61.cpp
@@ -1,41 +1,41 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
-int N, M;
-int ans = 0;
-vector<int> vec;
-
-void check(int level, int cur)
-{
-	if (level == N) {
-		if (cur == M) {
-			++ans;
-			return;
-		}
-		return;
-	}
-	
-	check(level + 1, cur + vec[level]);
-	check(level + 1, cur - vec[level]);
-	check(level + 1, cur);
-}
-
-int main()
-{
-	cin >> N >> M;
-
-	vec.resize(N);
-	for (int i = 0; i < N; ++i) {
-		cin >> vec[i];
-	}
-
-	check(0, 0);
-
-	if (ans > 0) {
-		cout << ans;
-	}
-	else {
-		cout << -1;
-	}
-}
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//int N, M;
+//int ans = 0;
+//vector<int> vec;
+//
+//void check(int level, int cur)
+//{
+//	if (level == N) {
+//		if (cur == M) {
+//			++ans;
+//			return;
+//		}
+//		return;
+//	}
+//	
+//	check(level + 1, cur + vec[level]);
+//	check(level + 1, cur - vec[level]);
+//	check(level + 1, cur);
+//}
+//
+//int main()
+//{
+//	cin >> N >> M;
+//
+//	vec.resize(N);
+//	for (int i = 0; i < N; ++i) {
+//		cin >> vec[i];
+//	}
+//
+//	check(0, 0);
+//
+//	if (ans > 0) {
+//		cout << ans;
+//	}
+//	else {
+//		cout << -1;
+//	}
+//}
---
+++ Algorithm/CodingTest_Inflearn/62.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void mergeAndSort(int arr[], int left, int right)
+{
+	int indexL, indexR, index;
+	int mid = (left + right) >> 1;
+	
+	indexL = left;
+	indexR = mid + 1;
+	index = left;
+
+	int temp[10] = { 0, };
+	while (indexL <= mid && indexR <= right)
+	{
+		temp[index++] = arr[indexL] < arr[indexR] ? arr[indexL++] : arr[indexR++];
+	}
+
+
+	for (int i = indexL; i <= mid; ++i)
+	{
+		temp[index++] = arr[i];
+	}
+
+	for (int i = indexR; i <= right; ++i)
+	{
+		temp[index++] = arr[i];
+	}
+
+	for (int i = left; i <= right; ++i)
+	{
+		arr[i] = temp[i];
+	}
+}
+
+void divide(int arr[], int left, int right)
+{
+	if (left >= right) { return; }
+
+	int mid = (left + right) / 2;
+	divide(arr, left, mid);
+	divide(arr, mid + 1, right);
+
+	mergeAndSort(arr, left, right);
+}
+
+int main()
+{
+	int N = 8;
+	int list[] = {7, 6, 3, 1, 5, 2, 4, 8};
+
+	divide(list, 0, N - 1);
+
+	for (int num : list)
+	{
+		cout << num << " ";
+	}
+}
