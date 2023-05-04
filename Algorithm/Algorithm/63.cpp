ab43169e244cd4ebbcfcff589a18695753a63982
scoke0801 <scoke0801@gmail.com>
2023-05-04 21:04:36 +0900

    64

--- Algorithm/Algorithm/63.cpp
+++ Algorithm/Algorithm/63.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main()
-{
-	int N, M;
-	cin >> N >> M;
-
-	vector< vector<int> > vec(N, vector<int>(N));
-
-	for (int i = 0; i < M; ++i) {
-		int col, row, cost;
-		cin >> col >> row >> cost;
-		vec[col - 1][row - 1] = cost;
-	}
-
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			cout << vec[i][j] << " ";
-		}
-		cout << "\n";
-	}
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int N, M;
+//	cin >> N >> M;
+//
+//	vector< vector<int> > vec(N, vector<int>(N));
+//
+//	for (int i = 0; i < M; ++i) {
+//		int col, row, cost;
+//		cin >> col >> row >> cost;
+//		vec[col - 1][row - 1] = cost;
+//	}
+//
+//	for (int i = 0; i < N; ++i) {
+//		for (int j = 0; j < N; ++j) {
+//			cout << vec[i][j] << " ";
+//		}
+//		cout << "\n";
+//	}
+//}
---
+++ Algorithm/CodingTest_Inflearn/64.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int N, M;
+int answer;
+bool visitied[21];
+
+void dfs(const vector<vector<int>>& vec, int from)
+{
+	if (from == N) {
+		++answer;
+		return;
+	}
+	if (visitied[from]) { return; }
+
+	visitied[from] = true;
+	for (int to : vec[from]) {
+		dfs(vec, to);
+	}
+	visitied[from] = false;
+}
+
+int main()
+{
+	int M;
+	cin >> N >> M;
+
+	vector<  vector<int> > vec(N + 1);
+
+	for (int i = 0; i < M; ++i) {
+		int from, to;
+		cin >> from >> to;
+
+		vec[from].push_back(to);
+	}
+
+	visitied[1] = true;
+	for (int from : vec[1]) {
+		dfs(vec, from);
+	}
+
+	cout << answer;
+}
