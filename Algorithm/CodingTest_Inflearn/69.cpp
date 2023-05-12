1861b3babd8f09a4747767519b865a802099c49a
scoke0801 <scoke0801@gmail.com>
2023-05-12 22:05:42 +0900

    70

--- Algorithm/CodingTest_Inflearn/69.cpp
+++ Algorithm/CodingTest_Inflearn/69.cpp
@@ -1,41 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
-
-/*
-1 2
-1 3
-2 4
-2 5
-3 6
-3 7
-*/
-
-vector<vector<int>> vec;
-queue<int> buffer;
-int main()
-{
-	vec.resize(8);
-	vec[1].push_back(2);
-	vec[1].push_back(3);
-
-	vec[2].push_back(4);
-	vec[2].push_back(5);
-
-	vec[3].push_back(6);
-	vec[3].push_back(7);
-
-	buffer.push(1);
-
-	while (!buffer.empty())
-	{
-		int node = buffer.front();
-		buffer.pop();
-
-		cout << node << " ";
-		for (int next : vec[node]) {
-			buffer.push(next);
-		}
-	}
-}
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//using namespace std;
+//
+///*
+//1 2
+//1 3
+//2 4
+//2 5
+//3 6
+//3 7
+//*/
+//
+//vector<vector<int>> vec;
+//queue<int> buffer;
+//int main()
+//{
+//	vec.resize(8);
+//	vec[1].push_back(2);
+//	vec[1].push_back(3);
+//
+//	vec[2].push_back(4);
+//	vec[2].push_back(5);
+//
+//	vec[3].push_back(6);
+//	vec[3].push_back(7);
+//
+//	buffer.push(1);
+//
+//	while (!buffer.empty())
+//	{
+//		int node = buffer.front();
+//		buffer.pop();
+//
+//		cout << node << " ";
+//		for (int next : vec[node]) {
+//			buffer.push(next);
+//		}
+//	}
+//}
---
+++ Algorithm/CodingTest_Inflearn/70.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct Element
+{
+	int to;
+	int level;
+};
+int N, M;
+vector<int> cost;
+vector<vector<Element>> vec;
+
+int main()
+{
+	cin >> N >> M;
+
+	vec.resize(N + 1);
+	cost.resize(N + 1);
+
+	for (int i = 0; i < M; ++i)
+	{
+		int from, to;
+		cin >> from >> to;
+		
+		vec[from].push_back({ to, 0 });
+	}	
+
+	queue<Element> buffer;
+	for (const Element& e : vec[1])
+	{
+		buffer.push({e.to, e.level + 1});
+	}
+
+	while (!buffer.empty())
+	{
+		Element e = buffer.front();
+		buffer.pop();
+
+		if (cost[e.to] != 0) { continue; }
+
+		cost[e.to] = e.level;
+
+		for (const Element& next : vec[e.to])
+		{
+			buffer.push({ next.to, e.level + 1 });
+		}
+	}
+
+	for (int i = 2; i <= N; ++i) {
+		cout << i << " : " << cost[i] << "\n";
+	}
+}
