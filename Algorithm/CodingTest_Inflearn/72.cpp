dd894f85738257524a502afadfbe86c9cd529368
scoke0801 <scoke0801@gmail.com>
2023-05-15 22:27:48 +0900

    73

--- Algorithm/CodingTest_Inflearn/72.cpp
+++ Algorithm/CodingTest_Inflearn/72.cpp
@@ -1,34 +1,34 @@
-#include <iostream>
-#include <queue>
-using namespace std;
-
-int main()
-{
-	int N, K;
-	cin >> N >> K;
-	
-	queue<int> buffer;
-	for (int i = K; i <= N; ++i) {
-		buffer.push(i);
-	}
-
-	for (int i = 1; i < K; ++i) {
-		buffer.push(i);
-	}
-
-	int count = K;
-	while (buffer.size() > 1)
-	{
-		for (int i = count; i < K - 1; ++i)
-		{
-			int front = buffer.front();
-			buffer.pop();
-			buffer.push(front);
-		}
-		
-		buffer.pop();
-		count = 0;
-	}
-
-	cout << buffer.front();
-}
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//int main()
+//{
+//	int N, K;
+//	cin >> N >> K;
+//	
+//	queue<int> buffer;
+//	for (int i = K; i <= N; ++i) {
+//		buffer.push(i);
+//	}
+//
+//	for (int i = 1; i < K; ++i) {
+//		buffer.push(i);
+//	}
+//
+//	int count = K;
+//	while (buffer.size() > 1)
+//	{
+//		for (int i = count; i < K - 1; ++i)
+//		{
+//			int front = buffer.front();
+//			buffer.pop();
+//			buffer.push(front);
+//		}
+//		
+//		buffer.pop();
+//		count = 0;
+//	}
+//
+//	cout << buffer.front();
+//}
