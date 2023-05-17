e5b5fd59bcf53540825c833ade70ef6c197e25cf
scoke0801 <scoke0801@gmail.com>
2023-05-17 21:02:14 +0900

    75

--- Algorithm/CodingTest_Inflearn/74.cpp
+++ Algorithm/CodingTest_Inflearn/74.cpp
@@ -1,39 +1,39 @@
-#include <iostream>
-#include <queue>
-using namespace std;
-
-int main()
-{
-	priority_queue<int, vector<int>, std::greater<int>> pq;
-	std::queue<int> input;
-
-	while (true)
-	{
-		int num;
-		cin >> num;
-
-		if (num == -1) {
-			break;
-		}
-
-		input.push(num);
-	}
-
-	while (!input.empty()) {
-		int num = input.front();
-		input.pop();
-		
-		if (num == 0) {
-			if (pq.empty()) {
-				cout << -1 << "\n";
-			}
-			else {
-				cout << pq.top() << "\n";
-				pq.pop();
-			}
-		}
-		else {
-			pq.push(num);
-		}
-	}
-}
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//int main()
+//{
+//	priority_queue<int, vector<int>, std::greater<int>> pq;
+//	std::queue<int> input;
+//
+//	while (true)
+//	{
+//		int num;
+//		cin >> num;
+//
+//		if (num == -1) {
+//			break;
+//		}
+//
+//		input.push(num);
+//	}
+//
+//	while (!input.empty()) {
+//		int num = input.front();
+//		input.pop();
+//		
+//		if (num == 0) {
+//			if (pq.empty()) {
+//				cout << -1 << "\n";
+//			}
+//			else {
+//				cout << pq.top() << "\n";
+//				pq.pop();
+//			}
+//		}
+//		else {
+//			pq.push(num);
+//		}
+//	}
+//}
