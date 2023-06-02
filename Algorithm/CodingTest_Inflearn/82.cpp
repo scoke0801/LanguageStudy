32a419b3088f518434be4569539abaa270eaaaa9
scoke0801 <scoke0801@gmail.com>
2023-06-02 21:22:14 +0900

    82

---
+++ Algorithm/CodingTest_Inflearn/82.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+int N, R;
+int ans;
+vector<int> vec;
+vector<bool> visited;
+vector<int> order;
+
+void dfs(int cur, int level)
+{
+	if (level == R) {
+		++ans;
+
+		for (int num : order) {
+			cout << num << " ";
+		}
+
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < N; ++i) {
+		if (visited[i]) { continue; }
+		visited[i] = true;
+
+		order.push_back(vec[i]);
+		dfs(vec[i], level + 1);
+		order.pop_back();
+
+		visited[i] = false;
+	}
+}
+
+int main()
+{
+	cin >> N >> R;
+
+	vec.resize(N);
+	visited.resize(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> vec[i];
+	}
+
+	std::sort(vec.begin(), vec.end());
+	for (int i = 0; i < N; ++i) {
+		visited[i] = true;
+		order.push_back(vec[i]);
+		dfs(vec[i], 1);
+		order.pop_back();
+		visited[i] = false;
+	}
+
+	cout << ans;
+}
