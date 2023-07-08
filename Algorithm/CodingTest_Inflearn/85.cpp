ecae18ff25c1db0772d8f413d82aa3cb88da90a9
scoke0801 <scoke0801@gmail.com>
2023-07-08 21:43:22 +0900

    86

--- Algorithm/CodingTest_Inflearn/85.cpp
+++ Algorithm/CodingTest_Inflearn/85.cpp
@@ -1,64 +1,64 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<int> nums; 
-
-vector<int> used_op(4);
-vector<int> op(4);		// + - X /
-
-int min_num = INT_MAX;
-int max_num = 0;
-int N;
-
-int Operate(int a, int b, int op)
-{
-	switch (op)
-	{
-	case 0: return a + b;
-	case 1:	return a - b;
-	case 2:	return a * b;
-	case 3:	return a / b;
-	}
-
-	return 0;
-}
-void DFS(int cur, int level)
-{
-	if (level == N) {
-		min_num = std::min(cur, min_num);
-		max_num = std::max(cur, max_num);
-		return;
-	}
-
-	for (int j = 0; j < 4; ++j)
-	{
-		if (used_op[j] >= op[j]) { continue; }
-		++used_op[j];
-
-		DFS(Operate(cur, nums[level], j), level + 1);
-
-		--used_op[j];
-	}
-}
-
-int main()
-{
-	cin >> N;
-	
-	for (int i = 0; i < N; ++i) {
-		int num;
-		cin >> num;
-
-		nums.push_back(num);
-	}
-
-	for (int i = 0; i < 4; ++i) {
-		cin >> op[i];
-	}
-
-	DFS(nums[0], 1);
-
-	cout << max_num << "\n";
-	cout << min_num << "\n";
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//vector<int> nums; 
+//
+//vector<int> used_op(4);
+//vector<int> op(4);		// + - X /
+//
+//int min_num = INT_MAX;
+//int max_num = 0;
+//int N;
+//
+//int Operate(int a, int b, int op)
+//{
+//	switch (op)
+//	{
+//	case 0: return a + b;
+//	case 1:	return a - b;
+//	case 2:	return a * b;
+//	case 3:	return a / b;
+//	}
+//
+//	return 0;
+//}
+//void DFS(int cur, int level)
+//{
+//	if (level == N) {
+//		min_num = std::min(cur, min_num);
+//		max_num = std::max(cur, max_num);
+//		return;
+//	}
+//
+//	for (int j = 0; j < 4; ++j)
+//	{
+//		if (used_op[j] >= op[j]) { continue; }
+//		++used_op[j];
+//
+//		DFS(Operate(cur, nums[level], j), level + 1);
+//
+//		--used_op[j];
+//	}
+//}
+//
+//int main()
+//{
+//	cin >> N;
+//	
+//	for (int i = 0; i < N; ++i) {
+//		int num;
+//		cin >> num;
+//
+//		nums.push_back(num);
+//	}
+//
+//	for (int i = 0; i < 4; ++i) {
+//		cin >> op[i];
+//	}
+//
+//	DFS(nums[0], 1);
+//
+//	cout << max_num << "\n";
+//	cout << min_num << "\n";
+//}
---
+++ Algorithm/CodingTest_Inflearn/86.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Coord {
+	int x, y;
+	Coord(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+};
+
+vector<Coord> shop;
+vector<Coord> house;
+vector<bool> visit;
+vector<int> buf(20, 0);
+int N, M;
+
+int ans = INT_MAX;
+void DFS(int cur, int level)
+{
+	if (level == M) {
+		int sum = 0;
+		for (int i = 0; i < house.size(); ++i) {
+			int x1 = house[i].x;
+			int y1 = house[i].y;
+			int dist = INT_MAX;
+			for (int j = 0; j < M; ++j) {
+				int x2 = shop[buf[j]].x;
+				int y2 = shop[buf[j]].y;
+				dist = std::min(dist, abs(x1 - x2) + abs(y1 - y2));
+			}
+			sum += dist;
+		}
+
+		ans = std::min(sum, ans);
+		return;
+	}
+	
+	for (int i = cur; i < shop.size(); ++i) {
+		buf[level] = i;
+		DFS(cur + 1, level + 1);
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+
+	visit.resize(N);
+	
+
+	int buffer;
+	for (int i = 0; i < N; ++i) 
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			cin >> buffer;
+
+			if (buffer == 1) {
+				house.push_back(Coord{ i,j });
+			}
+			else if (buffer == 2) {
+				shop.push_back(Coord{ i,j });
+			}
+		}
+	}
+
+	DFS(0, 0);
+
+	cout << ans << "\n";
+}
