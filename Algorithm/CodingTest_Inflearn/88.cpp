19f2c968376381837e6af39e288f0ee2acdf863a
scoke0801 <scoke0801@gmail.com>
2023-07-23 23:30:02 +0900

    89

--- Algorithm/CodingTest_Inflearn/88.cpp
+++ Algorithm/CodingTest_Inflearn/88.cpp
@@ -1,63 +1,63 @@
-#include <iostream>
-#include <stack>
-using namespace std;
-
-constexpr int BOARD_SIZE = 7;
-
-bool board[BOARD_SIZE][BOARD_SIZE];
-bool visited[BOARD_SIZE][BOARD_SIZE];
-
-// left top, right, down
-int x_dir[] = { -1,0,1,0 };
-int y_dir[] = { 0,-1,0,1 };
-
-struct Node {
-	int x, y;
-
-	Node(int x, int y) :x(x), y(y) {}
-};
-
-stack<Node> buffer;
-int moveCount;
-bool FindPath()
-{
-	buffer.emplace(0, 0);
-
-	while (!buffer.empty()) {
-		Node cur = buffer.top();
-		buffer.pop();
-
-		if (cur.x == BOARD_SIZE - 1 && cur.y == BOARD_SIZE - 1) {
-			return true;
-		}
-		++moveCount;
-		visited[cur.y][cur.x] = true;
-
-		for (int i = 0; i < 4; ++i) {
-			Node next{ cur.x + x_dir[i], cur.y + y_dir[i] };
-
-			if (next.x < 0 || next.y < 0) { continue; }
-			if (next.x >= BOARD_SIZE || next.y >= BOARD_SIZE) { continue; }
-			if (board[next.y][next.x]) { continue; }
-			if (visited[next.y][next.x]) { continue; }
-			buffer.push(std::move(next));
-		}
-	}
-
-	return false;
-}
-int main()
-{
-	for (int i = 0; i < BOARD_SIZE; ++i) {
-		for (int j = 0; j < BOARD_SIZE; ++j) {
-			cin >> board[i][j];
-		}
-	}
-
-	if (FindPath()) {
-		cout << moveCount;
-	}
-	else {
-		cout << -1;
-	}
-}
+//#include <iostream>
+//#include <stack>
+//using namespace std;
+//
+//constexpr int BOARD_SIZE = 7;
+//
+//bool board[BOARD_SIZE][BOARD_SIZE];
+//bool visited[BOARD_SIZE][BOARD_SIZE];
+//
+//// left top, right, down
+//int x_dir[] = { -1,0,1,0 };
+//int y_dir[] = { 0,-1,0,1 };
+//
+//struct Node {
+//	int x, y;
+//
+//	Node(int x, int y) :x(x), y(y) {}
+//};
+//
+//stack<Node> buffer;
+//int moveCount;
+//bool FindPath()
+//{
+//	buffer.emplace(0, 0);
+//
+//	while (!buffer.empty()) {
+//		Node cur = buffer.top();
+//		buffer.pop();
+//
+//		if (cur.x == BOARD_SIZE - 1 && cur.y == BOARD_SIZE - 1) {
+//			return true;
+//		}
+//		++moveCount;
+//		visited[cur.y][cur.x] = true;
+//
+//		for (int i = 0; i < 4; ++i) {
+//			Node next{ cur.x + x_dir[i], cur.y + y_dir[i] };
+//
+//			if (next.x < 0 || next.y < 0) { continue; }
+//			if (next.x >= BOARD_SIZE || next.y >= BOARD_SIZE) { continue; }
+//			if (board[next.y][next.x]) { continue; }
+//			if (visited[next.y][next.x]) { continue; }
+//			buffer.push(std::move(next));
+//		}
+//	}
+//
+//	return false;
+//}
+//int main()
+//{
+//	for (int i = 0; i < BOARD_SIZE; ++i) {
+//		for (int j = 0; j < BOARD_SIZE; ++j) {
+//			cin >> board[i][j];
+//		}
+//	}
+//
+//	if (FindPath()) {
+//		cout << moveCount;
+//	}
+//	else {
+//		cout << -1;
+//	}
+//}
---
+++ Algorithm/CodingTest_Inflearn/89.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+
+int x_dir[] = { -1, 0, 1, 0 };
+int y_dir[] = { 0,-1,0,1 };
+
+int M, N;
+
+struct RipeInfo
+{
+	int x, y;
+	int day;
+	RipeInfo(int x, int y, int day) : x(x), y(y), day(day) {}
+};
+
+vector<vector<int>> tomato;
+queue<RipeInfo> ripeInfo;
+
+void DoRipe()
+{
+	vector<vector<bool>> visited(N, vector<bool>(M));
+	
+	int ans = 0;
+	while (!ripeInfo.empty()) {
+		RipeInfo info = ripeInfo.front();
+		ripeInfo.pop();
+		visited[info.x][info.y] = true;
+
+		for (int i = 0; i < 4; ++i) {
+			int new_x = info.x + x_dir[i];
+			int new_y = info.y + y_dir[i];
+			if (new_x < 0 || new_y < 0) { continue; }
+			if (new_x >= N || new_y >= M) { continue; }
+			if (visited[new_x][new_y]) { continue; }
+
+			ans = std::max(info.day + 1, ans);
+			ripeInfo.emplace(new_x, new_y, info.day + 1);
+		}
+	}
+	cout << ans << "\n";
+}
+
+int main()
+{
+	cin >> M >> N;
+
+	tomato.resize(N);
+	
+	bool allTomatoRiped = true;
+	for (int i = 0; i < N; ++i) {
+		tomato[i].resize(M);
+		for (int j = 0; j < M; ++j) {
+			cin >> tomato[i][j];
+			if (tomato[i][j] == 1) {
+				ripeInfo.emplace(i, j, 0);
+			}
+			else {
+				allTomatoRiped = false;
+			}
+		}
+	}
+
+	if (ripeInfo.empty()) {
+		cout << -1;
+	}
+	else if (allTomatoRiped == true) {
+		cout << 0;
+	}
+	else
+	{
+		DoRipe();
+	}
+
+}
