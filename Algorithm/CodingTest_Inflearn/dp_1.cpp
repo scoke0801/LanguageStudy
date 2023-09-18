0e6d355d209eb93c8783f94aa0b5699f03b946d0
scoke0801 <scoke0801@gmail.com>
2023-09-18 21:28:26 +0900

    dp_1

---
+++ Algorithm/CodingTest_Inflearn/dp_1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int dp[100];
+
+void Init() {
+	for (int i = 0; i < _countof(dp); ++i) {
+		dp[i] = -1;
+	}
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 2;
+}
+
+void Calc(int N) {
+	if (dp[N] != -1) {
+		return;
+	}
+
+	for (int i = 3; i <= N; ++i) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+}
+
+int main()
+{
+	Init();
+
+	int N = 7;
+	Calc(N);
+
+	cout << dp[N];
+}
