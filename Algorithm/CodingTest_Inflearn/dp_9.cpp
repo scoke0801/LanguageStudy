c63d7ffd9ecaa20e3ed6f246efbe2bd5225b2a2e
scoke0801 <scoke0801@gmail.com>
2023-09-28 23:14:15 +0900

    dp_10

--- Algorithm/CodingTest_Inflearn/dp_9.cpp
+++ Algorithm/CodingTest_Inflearn/dp_9.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-
-int main()
-{
-	int num, target, weight, value;
-
-	cin >> num >> target;
-
-	vector<int> dp(target + 1, 0);
-
-	for (int i = 0; i < num; ++i) {
-		cin >> weight >> value;
-
-		for (int j = weight; j <= target; ++j)
-		{
-			dp[j] = std::max(dp[j], dp[j - weight] + value);
-		}
-	}
-
-	cout << dp[target];
-
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//
+//int main()
+//{
+//	int num, target, weight, value;
+//
+//	cin >> num >> target;
+//
+//	vector<int> dp(target + 1, 0);
+//
+//	for (int i = 0; i < num; ++i) {
+//		cin >> weight >> value;
+//
+//		for (int j = weight; j <= target; ++j)
+//		{
+//			dp[j] = std::max(dp[j], dp[j - weight] + value);
+//		}
+//	}
+//
+//	cout << dp[target];
+//
+//}
