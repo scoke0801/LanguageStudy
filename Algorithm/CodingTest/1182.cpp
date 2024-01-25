07c8c5392da4f46cd4ffe7f59111802e18090639
Hyun0801 <scoke0801@gmail.com>
2024-01-25 23:51:10 +0900

    14888

--- Algorithm/CodingTest/1182.cpp
+++ Algorithm/CodingTest/1182.cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-std::vector<int> arr;
-
-int N, S;
-int ans = 0; 
-void dfs(int sum, int level)
-{
-	if (level == N)
-	{
-		if (sum == S)
-		{
-			++ans;
-		}
-
-		return;
-	}
-
-
-	dfs(sum + arr[level], level + 1);
-
-	dfs(sum, level + 1);
-}
-
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	cin >> N >> S;
-
-	arr.resize(N); 
-	for (int i = 0; i < N; ++i)
-	{
-		cin >> arr[i];
-	}
-
-	dfs(0, 0);
-
-	if (S == 0) ans--;	//s가 0일때는 공집합 일때를 고려(-1)
-
-	std::cout << ans;
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//std::vector<int> arr;
+//
+//int N, S;
+//int ans = 0; 
+//void dfs(int sum, int level)
+//{
+//	if (level == N)
+//	{
+//		if (sum == S)
+//		{
+//			++ans;
+//		}
+//
+//		return;
+//	}
+//
+//
+//	dfs(sum + arr[level], level + 1);
+//
+//	dfs(sum, level + 1);
+//}
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//
+//	cin >> N >> S;
+//
+//	arr.resize(N); 
+//	for (int i = 0; i < N; ++i)
+//	{
+//		cin >> arr[i];
+//	}
+//
+//	dfs(0, 0);
+//
+//	if (S == 0) ans--;	//s가 0일때는 공집합 일때를 고려(-1)
+//
+//	std::cout << ans;
+//}
---
+++ Algorithm/CodingTest/14888.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <limits>
+using namespace std;
+
+int N;
+std::vector<int> arr;
+std::vector<int> oper(4);
+
+int min_ans = INT_MAX;
+int max_ans = INT_MIN;
+void dfs(int res, int level)
+{
+	if (level == N)
+	{
+		min_ans = std::min(min_ans, res);
+		max_ans = std::max(max_ans, res);
+		return;
+	}
+
+	for (int i = 0; i < 4; ++i) {
+		if (oper[i] == 0) { continue; }
+
+		--oper[i];
+
+		switch (i)
+		{
+		case 0:	dfs(res + arr[level], level + 1); break;
+		case 1:	dfs(res - arr[level], level + 1); break;
+		case 2:	dfs(res * arr[level], level + 1); break;
+		case 3: dfs(res / arr[level], level + 1); break;
+		default: break;
+		}
+
+		++oper[i];
+	}
+
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> N;
+
+	arr.resize(N);
+	for (int i = 0; i < N; ++i) {
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < 4; ++i) {
+		cin >> oper[i];
+	}
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (oper[i] == 0) { continue; }
+
+		dfs(arr[0], 1);
+	}
+
+	std::cout << max_ans << "\n" << min_ans << "\n";
+}
