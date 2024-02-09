dae92f3a70716c224f2653c05b2ec56b0eac8576
scoke0801 <scoke0801@gmail.com>
2024-02-09 17:11:39 +0900

    2529

--- Algorithm/CodingTest/16948.cpp
+++ Algorithm/CodingTest/16948.cpp
@@ -1,63 +1,63 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// #include <queue>
+// using namespace std;
 
 
-struct Data
-{
-	int x, y;
-	int level;
+// struct Data
+// {
+// 	int x, y;
+// 	int level;
 
-	Data(int x, int y, int level) :x(x), y(y), level(level) {}
-};
+// 	Data(int x, int y, int level) :x(x), y(y), level(level) {}
+// };
 
-int dx[6] = { -2, -2, 0, 0, 2, 2 };
-int dy[6] = { -1, 1, -2,2,-1,1 };
-int main()
-{
-	int N;
+// int dx[6] = { -2, -2, 0, 0, 2, 2 };
+// int dy[6] = { -1, 1, -2,2,-1,1 };
+// int main()
+// {
+// 	int N;
 
-	cin >> N;
+// 	cin >> N;
 	
-	std::vector<std::vector<int>> board(N, std::vector<int>(N));
-	std::vector<std::vector<bool>> visit(N, std::vector<bool>(N));
+// 	std::vector<std::vector<int>> board(N, std::vector<int>(N));
+// 	std::vector<std::vector<bool>> visit(N, std::vector<bool>(N));
 
-	int startX, startY;
-	int endX, endY;
+// 	int startX, startY;
+// 	int endX, endY;
 
-	cin >> startX >> startY;
-	cin >> endX >> endY;
+// 	cin >> startX >> startY;
+// 	cin >> endX >> endY;
 
 	
-	std::queue<Data> queue;
-	queue.push(Data(startX, startY, 0));
+// 	std::queue<Data> queue;
+// 	queue.push(Data(startX, startY, 0));
 
-	int ans = -1;
-	while (queue.empty() == false)
-	{
-		Data cur = queue.front();
-		queue.pop();
+// 	int ans = -1;
+// 	while (queue.empty() == false)
+// 	{
+// 		Data cur = queue.front();
+// 		queue.pop();
 
-		if (cur.x == endX && cur.y == endY)
-		{
-			ans = cur.level;
-			break;
-		}
+// 		if (cur.x == endX && cur.y == endY)
+// 		{
+// 			ans = cur.level;
+// 			break;
+// 		}
 
-		for (int i = 0; i < 6; ++i)
-		{
-			int nx = cur.x + dx[i];
-			int ny = cur.y + dy[i];
+// 		for (int i = 0; i < 6; ++i)
+// 		{
+// 			int nx = cur.x + dx[i];
+// 			int ny = cur.y + dy[i];
 
-			if (nx < 0 || nx >= N) { continue; }
-			if (ny < 0 || ny >= N) { continue; }
-			if (visit[ny][nx]) { continue; }
-			visit[ny][nx] = true;
-			queue.push(Data(nx, ny, cur.level + 1));
-		}
+// 			if (nx < 0 || nx >= N) { continue; }
+// 			if (ny < 0 || ny >= N) { continue; }
+// 			if (visit[ny][nx]) { continue; }
+// 			visit[ny][nx] = true;
+// 			queue.push(Data(nx, ny, cur.level + 1));
+// 		}
 
-	}
+// 	}
 
-	std::cout << ans << "\n";
-}
+// 	std::cout << ans << "\n";
+// }
---
+++ Algorithm/CodingTest/2529.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int path[10];
+bool visited[10];
+std::vector<char> oper;
+int ans = 0;
+int K;
+
+std::string first;
+std::string last;
+void Dfs(int level)
+{
+    if(level == K +1)
+    {
+        std::string temp ="";
+        for(int i =0; i <= K; ++i)
+        {
+            temp += to_string(path[i]);
+        }
+        if(first.empty()){
+            first = temp;
+        }
+        last = temp;
+        return;
+    }
+
+    for(int i =0; i <= 9; ++i)
+    {
+        if(visited[i]){continue;}
+        
+        path[level] = i;
+        if(level == 0)
+        {
+            visited[i] = true;
+            Dfs(level +1);
+            visited[i] = false;
+            continue;        
+        }
+
+        if(oper[level-1] == '<')
+        {
+            if(path[level -1] >= i)
+            {
+                continue;
+            }
+        }
+        else if(oper[level-1] == '>')
+        {
+            if(path[level - 1] <= i )
+            {
+                continue;
+            }
+        }
+    
+        visited[i] = true;
+        Dfs(level + 1);
+        visited[i] = false;
+    }
+}
+
+int main()
+{
+    cin >> K;
+    oper.resize(K);
+    for(int i =0;i<K;++i)
+    {
+        cin >> oper[i];
+    }
+    std::fill(std::begin(path), std::end(path), -1);
+
+    Dfs(0);
+
+    std::cout << last << "\n";
+    std::cout << first << "\n";
+    return 0;
+}
