69974923371c9097dd9bdb0a039a9fe564750393
scoke0801 <scoke0801@gmail.com>
2024-03-04 19:15:13 +0900

    1629

---
+++ Algorithm/CodingTest/1629.cpp
@@ -0,0 +1,22 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int A, B, C;
+// long long Func(long long num)
+// {
+//     if(num == 1) {return A % C;}
+
+//     long long temp = Func(num / 2) % C;
+//     if(num % 2 == 0) return temp * temp % C;
+//     else return temp * temp % C  * A % C;
+// }
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     cin >> A >> B >> C;
+
+//     std::cout << Func(B);
+// }
