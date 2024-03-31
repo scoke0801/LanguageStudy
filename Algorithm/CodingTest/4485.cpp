e73d31f06e7d6e7e320f0c4f18cdffef580858c0
Hyun0801 <scoke0801@gmail.com>
2024-03-31 20:26:32 +0900

    4485 / 5575

--- Algorithm/CodingTest/4485.cpp
+++ Algorithm/CodingTest/4485.cpp
@@ -1,79 +1,76 @@
-#include <queue>
-#include <iostream>
-#include <vector>
-#include <climits>
-using namespace std;
-
-int board[126][126];
-int dist[126][126];
-
-void Init()
-{
-    for(int i = 0; i < 126; ++i)
-    {
-        for(int j = 0; j < 126; ++j)
-        {
-            dist[i][j] = INT_MAX;
-            board[i][j] = 0;
-        }
-    }
-}
-
-int dx[4] = {-1,1,0,0};
-int dy[4] = {0,0,-1,1};
-
-int main()
-{
-    int T;
-    cin >> T;
-
-    int idx = 1;
-    while(idx > T)
-    {
-        int N;
-        cin >> N;
-        if(N == 0){break;}
-
-        Init();
-
-        for(int i = 0; i < N; ++i)
-        {
-            for(int j = 0; j < N; ++j)
-            {
-                cin >> board[i][j];            
-            }
-        }
-
-        dist[0][0] = board[0][0];    
-        std::priority_queue<std::pair<int ,std::pair<int,int>>> pq;
-        pq.push(std::make_pair( -1 * board[0][0], std::make_pair(0,0)));
-
-        while(pq.empty() == false)
-        {
-            int x = pq.top().second.first;
-            int y = pq.top().second.second;
-            pq.pop();
-
-            for(int i = 0; i < 4; ++i)
-            {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                if(nx < 0 || nx >= N || ny < 0 || ny >= N){continue;}
-                
-                int cost = board[ny][nx];
-                int before_cost = dist[ny][nx];
-                int new_cost = dist[y][x] + cost;
-                
-                if(before_cost > new_cost)
-                {
-                    dist[y][x] = new_cost;
-                    pq.push(std::make_pair(-1 * new_cost, std::make_pair(nx,ny)));
-                }
-            } 
-        }
-        std::cout << "Problem " << idx++ << ": " << dist[N-1][N-1] << "\n";
-    }
-
-    return 0;
-}
+//#include <queue>
+//#include <iostream>
+//#include <vector>
+//#include <climits>
+//using namespace std;
+//
+//int board[126][126];
+//int dist[126][126];
+//
+//void Init()
+//{
+//    for(int i = 0; i < 126; ++i)
+//    {
+//        for(int j = 0; j < 126; ++j)
+//        {
+//            dist[i][j] = INT_MAX;
+//            board[i][j] = 0;
+//        }
+//    }
+//}
+//
+//int dx[4] = {-1,1,0,0};
+//int dy[4] = {0,0,-1,1};
+//
+//int main()
+//{  
+//    int idx = 1;
+//    while (true)
+//    {
+//        int N;
+//        cin >> N;
+//        if(N == 0){break;}
+//
+//        Init();
+//
+//        for(int i = 0; i < N; ++i)
+//        {
+//            for(int j = 0; j < N; ++j)
+//            {
+//                cin >> board[i][j];            
+//            }
+//        }
+//
+//        dist[0][0] = board[0][0];    
+//        std::priority_queue<std::pair<int ,std::pair<int,int>>> pq;
+//        pq.push(std::make_pair( -1 * board[0][0], std::make_pair(0,0)));
+//
+//        while(pq.empty() == false)
+//        {
+//            int x = pq.top().second.first;
+//            int y = pq.top().second.second;
+//            pq.pop();
+//
+//            for(int i = 0; i < 4; ++i)
+//            {
+//                int nx = x + dx[i];
+//                int ny = y + dy[i];
+//                
+//                if(nx < 0 || nx >= N || ny < 0 || ny >= N){continue;}
+//                
+//                int cost = board[ny][nx];
+//                int before_cost = dist[ny][nx];
+//                int new_cost = dist[y][x] + cost;
+//                
+//                if(before_cost > new_cost)
+//                {
+//                    dist[ny][nx] = new_cost;
+//                    pq.push(std::make_pair(-1 * new_cost, std::make_pair(nx,ny)));
+//                }
+//            } 
+//        }
+//        std::cout << "Problem " << idx++ << ": " << dist[N-1][N-1] << "\n";
+//    }
+//
+//    return 0;
+//}
---
+++ Algorithm/CodingTest/5575.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+ 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	 
+	for (int i = 0; i < 3; ++i)
+	{
+		int h, m, s, hh, mm, ss;
+		cin >> h >> m >> s >> hh >> mm >> ss;
+
+		s = ss - s;
+		m = mm - m;
+		h = hh - h;
+
+		if (s < 0)
+		{
+			s = 60 + s;
+			m = m - 1;
+		}
+
+		if (m < 0)
+		{
+			m = 60 + m;
+			h = h - 1;
+		}
+
+		if (h < 0)
+		{
+			h = 24 - h;
+		}
+
+		std::cout << h << " " << m << " " << s << "\n";
+	}
+
+	return 0;
+}
