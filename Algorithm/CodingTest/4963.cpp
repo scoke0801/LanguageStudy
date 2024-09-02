c07a7383f98b592ab27be7b57769e8c15b09c7be
Hyun0801 <scoke0801@gmail.com>
2024-09-02 22:11:24 +0900

    4963

---
+++ Algorithm/CodingTest/4963.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//int main()
+//{
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(nullptr);
+//	cout.tie(nullptr);
+//
+//	while (true)
+//	{
+//		int W, H;
+//		cin >> W >> H;
+//	
+//		if (W == 0 && H == 0) { break; }
+//	}
+//
+//	return 0;
+//}
