51d23d3f46bf9604274b51e411fc7d3c85a48522
Hyun0801 <scoke0801@gmail.com>
2025-10-29 22:19:45 +0900

    2166

--- Algorithm/CodingTest/sibal.cpp
+++ Algorithm/CodingTest/sibal.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-
-using namespace std;
-
-class A
-{
-public:
-	virtual void Test()
-	{
-		std::cout << "A" << "\n";
-	}
-};
-
-class B : public A
-{
-public:
-	void Test() final
-	{
-		std::cout << "B" << "\n";
-	}
-};
-int main()
-{
-	A* tt = new B();
-
-	tt->Test();
-
-	return 0;
-}
+// #include <iostream>
+//
+// using namespace std;
+//
+// class A
+// {
+// public:
+// 	virtual void Test()
+// 	{
+// 		std::cout << "A" << "\n";
+// 	}
+// };
+//
+// class B : public A
+// {
+// public:
+// 	void Test() final
+// 	{
+// 		std::cout << "B" << "\n";
+// 	}
+// };
+// int main()
+// {
+// 	A* tt = new B();
+//
+// 	tt->Test();
+//
+// 	return 0;
+// }
--- CodingTest/CodingTest/makeLower.cpp
+++ CodingTest/CodingTest/makeLower.cpp
@@ -2,4 +2,4 @@
 #include<vector>
 #include <algorithm>
 using namespace std;
- 
+    
