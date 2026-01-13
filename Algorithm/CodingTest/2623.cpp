db5ff7054db6f953405cb741035f07e7a681caa1
Hyun0801 <scoke0801@gmail.com>
2026-01-13 23:24:48 +0900

    27172

--- Algorithm/CodingTest/2623.cpp
+++ Algorithm/CodingTest/2623.cpp
@@ -1,74 +1,74 @@
-﻿#include <iostream>
-#include <queue>
-#include <vector>
-using namespace std;
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int N, M;
-    cin >> N >> M;
-    
-    vector<vector<int>> graph(N + 1);
-    vector<int> indegree(N + 1, 0);
-    
-    for (int i = 0; i < M; ++i)
-    {
-        int A;
-        cin >> A;
-        vector<int> order(A);
-        for (int j = 0; j < A; ++j)
-        {
-            cin >> order[j];
-        }
-
-        for (int j = 0; j < A - 1; ++j)
-        {
-            graph[order[j]].push_back(order[j + 1]);
-            indegree[order[j + 1]]++;
-        }
-    }
-
-    queue<int> q;
-    vector<int> result;
-    
-    for (int i = 1; i <= N; i++)
-    {
-        if (indegree[i] == 0)
-        {
-            q.push(i);
-        }
-    }
-    
-    while (!q.empty())
-    {
-        int cur = q.front();
-        q.pop();
-        result.push_back(cur);
-        
-        for (int next : graph[cur])
-        {
-            indegree[next]--;
-            if (indegree[next] == 0)
-            {
-                q.push(next);
-            }
-        }
-    }
-    
-    if (result.size() != N)
-    {
-        cout << 0 << '\n';
-    }
-    else
-    {
-        for (int n : result)
-        {
-            cout << n << '\n';
-        }
-    }
-    
-    return 0;
-}
+﻿// #include <iostream>
+// #include <queue>
+// #include <vector>
+// using namespace std;
+//
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     
+//     int N, M;
+//     cin >> N >> M;
+//     
+//     vector<vector<int>> graph(N + 1);
+//     vector<int> indegree(N + 1, 0);
+//     
+//     for (int i = 0; i < M; ++i)
+//     {
+//         int cnt;
+//         cin >> cnt;
+//     
+//         int prev, cur;
+//         cin >> prev;
+//     
+//         for (int j = 1; j < cnt; j++)
+//         {
+//             cin >> cur;
+//             graph[prev].push_back(cur);
+//             indegree[cur]++;
+//             prev = cur;
+//         }
+//     }
+//
+//     queue<int> q;
+//     vector<int> result;
+//     
+//     for (int i = 1; i <= N; i++)
+//     {
+//         if (indegree[i] == 0)
+//         {
+//             q.push(i);
+//         }
+//     }
+//     
+//     while (!q.empty())
+//     {
+//         int cur = q.front();
+//         q.pop();
+//         result.push_back(cur);
+//         
+//         for (int next : graph[cur])
+//         {
+//             indegree[next]--;
+//             if (indegree[next] == 0)
+//             {
+//                 q.push(next);
+//             }
+//         }
+//     }
+//     
+//     if (result.size() != N)
+//     {
+//         cout << 0 << '\n';
+//     }
+//     else
+//     {
+//         for (int n : result)
+//         {
+//             cout << n << '\n';
+//         }
+//     }
+//     
+//     return 0;
+// }
---
+++ Algorithm/CodingTest/27172.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N;
+    cin >> N;
+    
+    const int MAX = 1000001;
+    vector<int> cards(N);
+    vector<bool> exists(MAX, false);
+    vector<int> score(MAX, 0);
+    
+    for (int i = 0; i < N; i++)
+    {
+        cin >> cards[i];
+        exists[cards[i]] = true;
+    }
+    
+    for (int i = 1; i < MAX; i++)
+    {
+        if (!exists[i]) continue;
+        
+        for (int multiple = 2 * i; multiple < MAX; multiple += i)
+        {
+            if (exists[multiple])
+            {
+                score[i]++;
+                score[multiple]--;
+            }
+        }
+    }
+    
+    for (int i = 0; i < N; i++)
+    {
+        cout << score[cards[i]];
+        if (i < N - 1) cout << " ";
+    }
+    cout << "\n";
+    
+    return 0;
+}
