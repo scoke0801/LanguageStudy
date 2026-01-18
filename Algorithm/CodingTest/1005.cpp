0cd5b49a21529b23abc1090440bf6fa3150eaf11
Hyun0801 <scoke0801@gmail.com>
2026-01-18 12:55:58 +0900

    2473

--- Algorithm/CodingTest/1005.cpp
+++ Algorithm/CodingTest/1005.cpp
@@ -1,69 +1,69 @@
-﻿#include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int T;
-    cin >> T;
-
-    for (int i = 0; i < T; ++i)
-    {
-        int N, K;
-        cin >> N >> K;
-
-        vector<int> buildTime(N+1);
-        for (int j = 1; j <= N; ++j)
-        {
-            cin >> buildTime[j];
-        }
-
-        vector<vector<int>> graph(N + 1);
-        vector<int> indegress(N+1, 0);
-        for (int j = 0; j < K; ++j)
-        {
-            int from, to;
-            cin >> from >> to;
-
-            graph[from].push_back(to);
-            indegress[to]++;
-        }
-        int W;
-        cin >> W;
-
-        vector<int> dp(N+1, 0);
-        queue<int> q;
-        for (int j = 1; j <= N; ++j)
-        {
-            if (indegress[j] == 0)
-            {
-                q.push(j);
-                dp[j] = buildTime[j];
-            }
-        }
-
-        while (!q.empty())
-        {
-            int current = q.front();
-            q.pop();
-            
-            for(int next : graph[current])
-            {
-                dp[next] = max(dp[next], dp[current] + buildTime[next]);
-                
-                indegress[next]--;
-                
-                if(indegress[next] == 0)
-                {
-                    q.push(next);
-                }
-            }
-        }
-
-        cout << dp[W] << '\n';
-    }
-}
+﻿// #include <iostream>
+// #include <vector>
+// #include <queue>
+// using namespace std;
+//
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//
+//     int T;
+//     cin >> T;
+//
+//     for (int i = 0; i < T; ++i)
+//     {
+//         int N, K;
+//         cin >> N >> K;
+//
+//         vector<int> buildTime(N+1);
+//         for (int j = 1; j <= N; ++j)
+//         {
+//             cin >> buildTime[j];
+//         }
+//
+//         vector<vector<int>> graph(N + 1);
+//         vector<int> indegress(N+1, 0);
+//         for (int j = 0; j < K; ++j)
+//         {
+//             int from, to;
+//             cin >> from >> to;
+//
+//             graph[from].push_back(to);
+//             indegress[to]++;
+//         }
+//         int W;
+//         cin >> W;
+//
+//         vector<int> dp(N+1, 0);
+//         queue<int> q;
+//         for (int j = 1; j <= N; ++j)
+//         {
+//             if (indegress[j] == 0)
+//             {
+//                 q.push(j);
+//                 dp[j] = buildTime[j];
+//             }
+//         }
+//
+//         while (!q.empty())
+//         {
+//             int current = q.front();
+//             q.pop();
+//             
+//             for(int next : graph[current])
+//             {
+//                 dp[next] = max(dp[next], dp[current] + buildTime[next]);
+//                 
+//                 indegress[next]--;
+//                 
+//                 if(indegress[next] == 0)
+//                 {
+//                     q.push(next);
+//                 }
+//             }
+//         }
+//
+//         cout << dp[W] << '\n';
+//     }
+// }
---
+++ Algorithm/CodingTest/2473.cpp
@@ -0,0 +1,56 @@
+﻿// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// #include <cmath>
+//
+// using namespace std;
+//
+// int main()
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//
+//     int N;
+//     cin >> N;
+//
+//     vector<long long> v(N);
+//
+//     for (int i = 0; i < N; ++i)
+//     {
+//         cin >> v[i];
+//     }
+//
+//     sort(v.begin(), v.end());
+//
+//     long long minValue = 5000000000;
+//     vector<long long> answer(3);
+//
+//     for (int i = 0; i < N; ++i)
+//     {
+//         int left = i + 1;
+//         int right = N - 1;
+//
+//         while (left < right)
+//         {
+//             long long sum = v[i] + v[left] + v[right];
+//
+//             if (abs(sum) < abs(minValue))
+//             {
+//                 minValue = sum;
+//                 answer[0] = v[i];
+//                 answer[1] = v[left];
+//                 answer[2] = v[right];
+//             }
+//
+//             if (sum == 0)
+//                 break;
+//
+//             if (sum > 0)
+//                 right--;
+//             else if (sum < 0)
+//                 left++;
+//         }
+//     }
+//
+//     cout << answer[0] << " " << answer[1] << " " << answer[2] << "\n";
+// }
